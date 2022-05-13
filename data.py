7e33003d067c71902ecc98baa29da47f52ad65eb
Maria Arv <79372611+mariaarv88@users.noreply.github.com>
2022-05-13 21:59:15 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from scipy import signal
@@ -21,11 +20,11 @@ def Rt(df,ix0=-1,smooth="10D",k=2209/290,t=29/47):
         s+=dist.pdf(ix)*Irm[ix0-ix]
         #print(ix,ix0-ix,Irm[ix0-ix],Irm[ix0]/s) 
     return Irm[ix0]/s
-  
+
 df["Rt"]=np.nan
 for i in range(len(df)):
     df["Rt"].iloc[i]=Rt(df,i)
-    
+
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
@@ -34,8 +33,6 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(layout="wide")
 st.title('Greece covid analytics Dashboard')
-
-
 
 
 # Index(['new_cases', 'confirmed', 'new_deaths', 'total_deaths', 'new_tests',
@@ -61,16 +58,16 @@ Rows={"Cases and Testing":["New Cases","Rt","New Tests","New Positive Tests"],
 for Row in Rows: #Row is every key in dictionary Rows 
     # loop 0: Row="Cases and Testing" , Rows[Row]=["New Cases","Rt","New Tests","New Positive Tests"]
     cols = st.columns(tuple([0.5]+[1]*len(Rows[Row]))) #tuple([0.5]+[1]*len(Rows[Row]))= (0.5,1,1,1,1)
-    
+
     with cols[0]:
         st.markdown(Row)
-        
-    #zip
+
+    #zip 
     #for i,j,k in zip(['a','b','c'],[1,2,3],[7,8,9]):
     #i='a',j=1,k=7
     #'b',2,8
     #'c',3,9
-    
+
     for ci,label in zip(cols[1:],Rows[Row]):
         #loop 0: ci = cols[1], label="New Cases"
         if label != '':
@@ -79,7 +76,7 @@ for Row in Rows: #Row is every key in dictionary Rows
                 val=df.iloc[-1][info]
                 dif=df.iloc[-1][info]-df.iloc[-2][info]
             else: 
-                
+
                 #it is only about Hospitalizations, which is around one day before
                 notna=df[~pd.isna(df[info])] #temporary dataframe (with random name notna), without nan values
                 #pd.isna(df[info]) is True where the dataframe is NaN
@@ -98,6 +95,7 @@ for Row in Rows: #Row is every key in dictionary Rows
                 ci.metric(label=label,value= round(val,2), delta = str(round(dif,2)), delta_color = 'inverse')
 
 
+
 row_spacer_start, row1, row2, row_spacer_end  = st.columns((0.1, 1.0, 6.4, 0.1))
 
 with row1:
@@ -105,38 +103,37 @@ with row1:
     plot_value = st.selectbox ("Variable", list(value_labels.keys()), key = 'value_key') #take all the keys from value_labels dictionary
     plot_value2 = st.selectbox ("Second Variable", [None]+list(value_labels.keys()), key = 'value_key')
     smooth = st.checkbox("Add smooth curve")
-   
-    log = st.checkbox("Use log scale")
-    
+
+
 with row2:    
     sec= not (plot_value2 is None) #True or False if there is a second plot
-    
+
     fig = make_subplots(specs=[[{"secondary_y": sec}]]) #plotly function, define fig which will be show at iser
-    
+
     x1=df.index #abbreviation for dates
     y1=df[value_labels[plot_value]] #abbreviation for ploting values, translate from shown names to column names (from value_labels dictionary)
-    
+
     #fig1= px.bar(df,x = x1, y=value_labels[plot_value])#,log_y=log)
-    
+
     fig1= px.bar(df,x = x1, y=y1) #bar plot named as fig1
-    
+
     fig.add_traces(fig1.data) #add to the fig (what is going to be show to the user) the fig1
-    
-    
+
+
     fig.layout.yaxis.title=plot_value #add label
-    
+
     if smooth:
         # Create temprary rolling average dataframe
         ys1= df.rolling("7D").mean()[value_labels[plot_value]]
         figs=px.line(x = x1, y=ys1)#,log_y=log)
         fig.add_traces(figs.data)   #add figs to the fig (what we will show at the end)     
-        
+
     if sec:
         x2=df.index
         y2=df[value_labels[plot_value2]]
         figsec=px.line(x = x2, y=y2)#,log_y=log)
         figsec.update_traces(yaxis="y2")
-        
+
         fig.add_traces(figsec.data) #add figsec to the fig (what we will show at the end) 
         fig.layout.yaxis2.title=plot_value2
 
